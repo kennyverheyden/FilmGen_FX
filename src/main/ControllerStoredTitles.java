ec8cb40d177e593c620b14522b5476c67df67f35
Kenny Verheyden <kenny.verheyden@gmail.com>
2023-01-19 10:01:53 +0100

    Commit

--- src/main/ControllerStoredTitles.java
+++ src/main/ControllerStoredTitles.java
@@ -5,11 +5,14 @@ import classes.FilmTitle;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
@@ -27,12 +30,19 @@ public class ControllerStoredTitles {
 	@FXML
 	private TableColumn<?, ?> storedTitles;
 	
+    @FXML
+    private TableView<String> tableTitles;
+	
 	FilmTitle title = new FilmTitle();
+	private ObservableList<String> titleList = FXCollections.observableArrayList();
 	
 	public void initialize()
 	{
-		ArrayList<String> content =  title.readStoredTitle();
+		//titleList =  (ObservableList<title>) title.readStoredTitle();
 		
+		tableTitles.setItems(titleList);
+		
+		//storedTitles.setCellFactory(rowValue -> rowValue.getValue().);
 	}
 	
 
